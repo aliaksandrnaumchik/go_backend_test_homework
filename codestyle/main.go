bc65a1653699a49e3e789ef4f1546a87de4145d1
anaumcik <naumcik.inno.co@gmail.com>
2025-08-08 18:21:18 +0300

    refactor checkstyle

--- codestyle/main.go
+++ codestyle/main.go
@@ -5,19 +5,19 @@ import (
 	"math"
 )
 
-var Message = `Добро пожаловать в самую лучшую программу для вычисления 
+const Message = `Добро пожаловать в самую лучшую программу для вычисления 
 квадратного корня из заданного числа`
 
-func calculate_square_root(my_number float64) float64 {
-	return math.Sqrt(my_number)
+func calculateSquareRoot(number float64) float64 {
+	return math.Sqrt(number)
 }
 
 func calc(number float64) {
 	if number < 0 {
 		return
-	} else {
-		fmt.Println("Мы вычислили квадратный корень из введённого вами числа. Это будет:", CalculateSquareRoot(number))
 	}
+
+	fmt.Println("Мы вычислили квадратный корень из введённого вами числа. Это будет:", calculateSquareRoot(number))
 }
 
 func main() {
